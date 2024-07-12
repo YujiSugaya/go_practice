d719369149b612c3a00dace5aced71d8bdb5eeff
YujiSugaya <sugaya.yuji@moneyforward.co.jp>
2024-07-12 20:59:31 +0900

    adding new file

--- Basics/named-results.go
+++ Basics/named-results.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+//変数にx,yという名前をつけて実施
 func split(sum int)(x,y int){
 	x = sum * 4 / 9
 	y = sum - x
---
+++ Basics/zero.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+	//0
+	var i int
+	//0
+	var f float64
+	//false
+	var b bool
+	//""
+	var s string
+	fmt.Printf("%v %v %v %q\n", i, f, b, s)
+}
