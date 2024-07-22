b1a0c47a877f54090e2bffbcc2da2bd88a355d1d
YujiSugaya <sugaya.yuji@moneyforward.co.jp>
2024-07-22 15:46:53 +0900

    adding a new file

--- Basics/More types: structs, slices, and maps./slices-of-slice.go
+++ Basics/More types: structs, slices, and maps./slices-of-slice.go
@@ -14,13 +14,20 @@ func main() {
 	}
 	fmt.Println(board)
 
+	//一行目の一番目
 	board[0][0] = "X"
+	//三行目の3番目
 	board[2][2] = "O"
+	//二行目の3番目
 	board[1][2] = "X"
+	//二行目の一番目
 	board[1][0] = "O"
+	//一行目の3番目
 	board[0][2] = "X"
 
+	//ボードの各行をループで処理します。
 	for i := 0; i < len(board); i++ {
+		//各行の要素を空白で区切って1つの文字列にします。
 		fmt.Printf("%s\n", strings.Join(board[i], " "))
 	}
 }
