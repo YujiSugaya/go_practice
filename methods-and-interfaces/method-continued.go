3088ea2105913926f5c6a6463cabbfba6c4c01b1
YujiSugaya <sugaya.yuji@moneyforward.co.jp>
2024-08-07 21:49:51 +0900

    added some comments

--- methods-and-interfaces/method-continued.go
+++ methods-and-interfaces/method-continued.go
@@ -5,16 +5,23 @@ import (
 	"math"
 )
 
+//Myfloatという新い方を定義している　float64の機能を持ちつつ独自のメソッドを追加することができる
 type MyFloat float64
 
+//fはレシーバ変数　MyFloat型に対してAbsメソッドを定義している1
 func (f MyFloat) Abs() float64 {
 	if f < 0 {
+		//0より小さい場合つまり負の値の場合はその値に-をかけて正の値にします。
 		return float64(-f)
 	}
+	//正の値の場合はそのまま返します。
 	return float64(f)
 }
 
 func main() {
+	// - ルート2がfに入っている　-1.4142
 	f := MyFloat(-math.Sqrt2)
+	//fのAbsメソッドを出力する
+	//今回は負の値が入っているので　正の値にして出力する
 	fmt.Println(f.Abs())
 }
